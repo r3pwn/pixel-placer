f7b05c78818e50b182fc712d9439d68a26f3e7a2
Greg Willard (r3pwn) <wickett06@gmail.com>
2024-05-01 01:36:07 -0400

    feat(canvas): add primitive color picker

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -4,6 +4,7 @@ import Pixel from "./Pixel";
 
 const NUM_ROWS = 10;
 const PIXELS_PER_ROW = 10;
+const COLOR_OPTIONS = ["#eb4034", "#8709db", "#eff702", "#ffffff", "#000000"];
 
 type Props = {
   readonly?: boolean;
@@ -17,7 +18,7 @@ type CanvasPixel = {
 
 export default function Canvas({ readonly }: Props) {
   const [pixels, setPixels] = useState([] as CanvasPixel[]);
-  const [currentColor, setCurrentColor] = useState("#eb4034");
+  const [currentColor, setCurrentColor] = useState(COLOR_OPTIONS[0]);
 
   useEffect(() => {
     let canvasPixels = new Array(NUM_ROWS * PIXELS_PER_ROW)
@@ -69,24 +70,46 @@ export default function Canvas({ readonly }: Props) {
   };
 
   return (
-    <ul
-      className="canvas"
-      style={{
-        display: "inline-grid",
-        gridTemplateColumns: "repeat(10, 1fr)",
-        gridTemplateRows: "repeat(10, 1fr)",
-      }}
-    >
-      {pixels.map((pixel, index) => (
-        <li key={index}>
-          <Pixel
-            color={pixel.color}
-            onClick={() => {
-              handleClick(pixel);
-            }}
-          />
-        </li>
-      ))}
-    </ul>
+    <>
+      {!readonly && (
+        <div className="color-options">
+          {COLOR_OPTIONS.map((color) => (
+            <input
+              key={color}
+              name="color_option"
+              value={color}
+              checked={color === currentColor}
+              type="radio"
+              style={{
+                width: 20,
+                height: 20,
+                accentColor: color,
+                borderRadius: "100%",
+              }}
+              onChange={(e) => setCurrentColor(e.target.value)}
+            />
+          ))}
+        </div>
+      )}
+      <ul
+        className="canvas"
+        style={{
+          display: "inline-grid",
+          gridTemplateColumns: "repeat(10, 1fr)",
+          gridTemplateRows: "repeat(10, 1fr)",
+        }}
+      >
+        {pixels.map((pixel, index) => (
+          <li key={index}>
+            <Pixel
+              color={pixel.color}
+              onClick={() => {
+                handleClick(pixel);
+              }}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
