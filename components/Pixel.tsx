46d5367d7ad77c22b7c9f3ceb266ad808bfc273b
Greg Willard (r3pwn) <wickett06@gmail.com>
2024-05-04 00:12:57 -0400

    chore(pixel): update to use new button component

--- components/Pixel.tsx
+++ components/Pixel.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { Popover } from '@headlessui/react'
-import ColorPicker from './ColorPicker';
-import Color from 'color';
-import { Float } from '@headlessui-float/react';
+import { Popover } from "@headlessui/react";
+import ColorPicker from "./ColorPicker";
+import Color from "color";
+import { Float } from "@headlessui-float/react";
+import { Button } from "./core/Button";
 
 type Props = {
   color: string;
@@ -22,34 +23,45 @@ export default function Pixel({ color, readonly, size, onSubmit }: Props) {
   const darkenedColor = colorObject.darken(0.2);
   if (readonly) {
     return (
-      <div className='canvas-pixel' style={{
-        backgroundColor: color,
-        width: size || defaultProps.size,
-        height: size || defaultProps.size,
-      }}></div>
-    )
+      <div
+        className="canvas-pixel"
+        style={{
+          backgroundColor: color,
+          width: size || defaultProps.size,
+          height: size || defaultProps.size,
+        }}
+      ></div>
+    );
   }
   return (
     <Popover className="relative">
-      <Float autoPlacement={
-        {
-          alignment: "start"
-        }
-      }
+      <Float
+        autoPlacement={{
+          alignment: "start",
+        }}
       >
-        <Popover.Button as="div"
+        <Popover.Button
+          as="div"
           className={`canvas-pixel hover:scale-110 hover:outline-1 hover:outline hover:relative hover:z-10 hover:shadow-lg outline-offset-[-1px] ui-open:scale-110 ui-open:outline-1 ui-open:outline ui-open:relative ui-open:z-10 ui-open:shadow-lg`}
           style={{
             backgroundColor: color,
             width: size || defaultProps.size,
             height: size || defaultProps.size,
-            outlineColor: darkenedColor.toString()
+            outlineColor: darkenedColor.toString(),
           }}
         />
-        <Popover.Panel className={"z-10 bg-zinc-800 p-2 rounded-md shadow-md flex flex-col"}>
+        <Popover.Panel
+          className={"z-10 bg-zinc-800 p-2 rounded-md shadow-md flex flex-col"}
+        >
           <h2>Choose the pixel color!</h2>
           <ColorPicker currentPixelColor={color} />
-          <button className='bg-cyan-800 hover:bg-cyan-900 px-2 py-1 mt-3 ml-auto rounded-md' onClick={() => onSubmit && onSubmit()}>Set Pixel Color</button>
+          <Button
+            className="mt-3 ml-auto"
+            type="button"
+            onClick={() => onSubmit && onSubmit()}
+          >
+            Set Pixel Color
+          </Button>
         </Popover.Panel>
       </Float>
     </Popover>
