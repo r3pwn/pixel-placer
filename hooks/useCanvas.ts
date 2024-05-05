111cb52f050291a2b96dc78ac856933399eb569e
Greg Willard (r3pwn) <wickett06@gmail.com>
2024-05-04 23:21:24 -0400

    chore: clean up "mirror" canvas references

--- hooks/useCanvas.ts
+++ hooks/useCanvas.ts
@@ -5,11 +5,7 @@ export const useCanvas = (width: number, height: number, scale = 1) => {
   const [canvasContext, setCanvasContext] = useState(
     null as CanvasRenderingContext2D | null
   );
-  const [mirrorContext, setMirrorContext] = useState(
-    null as CanvasRenderingContext2D | null
-  );
   const canvasRef = useRef(null as HTMLCanvasElement | null);
-  const mirrorRef = useRef(null as HTMLCanvasElement | null);
 
   useEffect(() => {
     const context = canvasRef?.current?.getContext("2d");
@@ -23,18 +19,6 @@ export const useCanvas = (width: number, height: number, scale = 1) => {
     // blank the canvas
     context.fillStyle = "#ffffff";
     context.fillRect(0, 0, width, height);
-
-    const mirrorContext = mirrorRef?.current?.getContext("2d");
-
-    if (!mirrorContext) {
-      return;
-    }
-
-    setMirrorContext(mirrorContext);
-
-    // blank the canvas
-    mirrorContext.fillStyle = "#ffffff";
-    mirrorContext.fillRect(0, 0, width, height);
   }, [width, height]);
 
   const flushCanvas = () => {
@@ -46,14 +30,6 @@ export const useCanvas = (width: number, height: number, scale = 1) => {
     // blank the canvas
     canvasContext.fillStyle = "#ffffff";
     canvasContext.fillRect(0, 0, width, height);
-
-    if (!mirrorContext) {
-      return;
-    }
-
-    // blank the canvas
-    mirrorContext.fillStyle = "#ffffff";
-    mirrorContext.fillRect(0, 0, width, height);
   };
 
   const drawToCanvas = (pixels: CanvasPixel[]) => {
@@ -65,18 +41,12 @@ export const useCanvas = (width: number, height: number, scale = 1) => {
     for (let pixel of pixels) {
       canvasContext.fillStyle = pixel.color;
       canvasContext.fillRect(pixel.x * scale, pixel.y * scale, scale, scale);
-
-      if (mirrorContext) {
-        mirrorContext.fillStyle = pixel.color;
-        mirrorContext.fillRect(pixel.x * scale, pixel.y * scale, scale, scale);
-      }
     }
   };
 
   return {
     canvasRef,
     canvasContext,
-    mirrorRef,
     flushCanvas,
     drawToCanvas,
   };
