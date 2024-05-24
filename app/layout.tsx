4694e78de0b11e3ca45338b188ab098ed4afe513
Greg Willard (r3pwn) <wickett06@gmail.com>
2024-05-24 19:07:38 -0400

    chore: rebrand to pixel-placer

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Pixel Place",
+  title: "Pixel Placer",
   description: "Place your pixels wherever you want!",
 };
 
